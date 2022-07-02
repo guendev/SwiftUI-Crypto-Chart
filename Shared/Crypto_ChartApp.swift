df6edf8f5ae6781369a9d6b0c77ee74d2ba9bcb2
coder <dnstylish@gmail.com>
2022-07-02 22:40:17 +0700

    Initial Commit

---
+++ Shared/Crypto_ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_ChartApp.swift
+//  Shared
+//
+//  Created by Yuan on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
